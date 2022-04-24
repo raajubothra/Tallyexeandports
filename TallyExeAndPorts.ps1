8c51e7ee9157e36418891a0bebf34388ffc37ee8
raajubothra <45957430+raajubothra@users.noreply.github.com>
2022-04-24 17:55:08 +0530

    Add files via upload

---
+++ TallyExeAndPorts.ps1
@@ -0,0 +1,15 @@
+﻿$TallyPrime = "C:\TallyPrime\tally.exe"
+$TallyGatewayServer = "C:\TallyPrime\tallygatewayserver.exe"
+$TallyLicensePort = "9999"
+$TallyOdbcPort    =  "9000"
+
+
+New-NetFirewallRule -Program $TallyPrime -Action Allow -Profile Any -DisplayName "Allow TallyPrime" -Description "Tally TallyPrime Application" -Direction Outbound
+
+New-NetFirewallRule -Program $TallyPrime -Action Allow -Profile Any -DisplayName "Allow TallyPrime" -Description "Tally TallyPrime Application" -Direction Inbound
+New-NetFirewallRule -Program $TallyGatewayServer -Action Allow -Profile Any -DisplayName "Allow Tally Gateway Server" -Description "Allow Tally GateWay Server for Licensing" -Direction Outbound
+New-NetFirewallRule -Program $TallyGatewayServer -Action Allow -Profile Any -DisplayName "Allow Tally Gateway Server" -Description "Allow Tally GateWay Server for Licensing" -Direction Inbound
+New-NetFirewallRule -DisplayName "Allow Tally License Port" -Direction Outbound  -Profile Any -Description "Allow TallyLicense Port" -LocalPort $TallyLicensePort -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName "Allow Tally License Port" -Direction Inbound  -Profile Any -Description "Allow TallyLicense Port" -LocalPort $TallyLicensePort -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName "Allow Tally ODBC Port" -Direction Inbound  -Profile Any -Description "Allow Tally ODBC Port" -LocalPort $TallyOdbcPort -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName "Allow Tally ODBC Port" -Direction Outbound  -Profile Any -Description "Allow Tally ODBC Port" -LocalPort $TallyOdbcPort -Protocol TCP -Action Allow
